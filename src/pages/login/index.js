c21fe9f7cb5d7a8cd6266cd27a0ef0188557b2d8
vijayyprajapati <vijayprajapativijay2@gmail.com>
2023-06-04 12:15:13 +0530

    Login Page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,36 +1,51 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import Styles from "./login.module.css";
 import { getLocalStorage, setLocalStorage } from "../../utils/localStorage";
+
 const Login = () => {
   const navigate = useNavigate();
-  const theme = getLocalStorage("theme");
-  if (theme) {
-    const root = document.querySelector(":root");
-    root.style.setProperty("--background", theme.background);
-    root.style.setProperty("--primary", theme.primary);
-    root.style.setProperty("--secondary", theme.secondary);
-  }
+
+  useEffect(() => {
+    const theme = getLocalStorage("theme");
+    if (theme) {
+      const { background, primary, secondary } = theme;
+      const root = document.querySelector(":root");
+      root.style.setProperty("--background", background);
+      root.style.setProperty("--primary", primary);
+      root.style.setProperty("--secondary", secondary);
+    }
+  }, []);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setLocalStorage("login-state", true);
     navigate("/chat/?delay=0.50", { replace: true });
   };
+
   return (
     <div className={Styles.loginpage}>
       <div className={Styles.logo}>
-        <img src="logo.png" alt="Wysa Logo" />
+        <img src="wysa.png" alt="Wysa Logo" />
       </div>
       <form onSubmit={handleSubmit}>
-        <input type="email" name="email" placeholder="Email" required />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          required
-        />
+        <label>
+         
+          <input type="email" name="email" placeholder="Email" required />
+        </label>
+        <label>
+         
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          />
+        </label>
         <button type="submit">Login</button>
       </form>
     </div>
   );
 };
+
 export default Login;
