1463fae592602c5a9e31fbb9019c48ca93fd3ced
vijayyprajapati <vijayprajapativijay2@gmail.com>
2023-06-05 09:52:21 +0530

    Added change of default theme

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,10 +1,25 @@
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import { getLocalStorage, setLocalStorage } from "../../utils/localStorage";
 import Styles from "./navbar.module.css";
+import { defaultTheme } from "../../Data/themes";
 
 const NavBar = ({ setOpen, open }) => {
   const navigate = useNavigate();
   const profileImage = getLocalStorage("login-data").profileImage;
+
+  const handleLogout = () => {
+    setLocalStorage("login-state", false);
+    setLocalStorage("theme", defaultTheme); // Reset theme to default
+
+    // Reset CSS variables to default theme
+    const root = document.querySelector(":root");
+    root.style.setProperty("--background", defaultTheme.background);
+    root.style.setProperty("--primary", defaultTheme.primary);
+    root.style.setProperty("--secondary", defaultTheme.secondary);
+
+    navigate("/login", { replace: true });
+  };
 
   return (
     <div className={Styles.navbar}>
@@ -20,13 +35,7 @@ const NavBar = ({ setOpen, open }) => {
         {profileImage && (
           <img className={Styles.profileImage} src={profileImage} alt="Profile" />
         )}
-        <div
-          className={Styles.logout}
-          onClick={() => {
-            setLocalStorage("login-state", false);
-            navigate("/login", { replace: true });
-          }}
-        >
+        <div className={Styles.logout} onClick={handleLogout}>
           <span className="material-symbols-outlined">logout</span>
         </div>
       </div>
